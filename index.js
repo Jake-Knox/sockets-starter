af91831dc7b9e9b0c256f368f781b918e9df3b64
jake-knox- <jakeknox11@hotmail.co.uk>
2023-05-02 15:05:42 +0100

    adding files

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    // socket.broadcast.emit('hi');
+
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+    
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
